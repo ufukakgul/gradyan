3eab65ee15d2ba84d3d004504e5ea0dbf4383aed
DESKTOP-HQNJO0S\akgul <ceufukakgul@gmail.com>
2021-10-17 11:13:59 +0300

    proje olusturuldu

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'Gradyan'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomRight,
+          stops: const [
+            0.1,
+            0.4,
+            0.8,
+            0.9,
+          ],
+          colors: [
+            Colors.blue.shade800,
+            Colors.blue.shade600,
+            Colors.red.shade200,
+            Colors.red.shade100,
+          ],
+        )),
+      ),
+    );
+  }
+}
